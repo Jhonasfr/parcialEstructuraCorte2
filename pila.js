7723f52271bb51c7a81d961957a70a9a45127268
Jhonasfr <jvinascocobo@gmail.com>
2026-04-17 17:12:47 -0500

    Agrega logica de pila con objetos

--- pila.js
+++ pila.js
@@ -1,1 +1,39 @@
-fr
+class Pila {
+  constructor() {
+    this.items = [];
+  }
+
+  agregar(objeto) {
+    this.items.push(objeto);
+    console.log("Elemento agregado a la pila:", objeto);
+  }
+
+  eliminar() {
+    if (this.estaVacia()) {
+      console.log("La pila está vacía");
+      return null;
+    }
+    const eliminado = this.items.pop();
+    console.log("Elemento eliminado de la pila:", eliminado);
+    return eliminado;
+  }
+
+  mostrar() {
+    console.log("Contenido actual de la pila:", this.items);
+  }
+
+  estaVacia() {
+    return this.items.length === 0;
+  }
+}
+
+// Pruebas
+const pila = new Pila();
+
+pila.agregar({ nombre: "Orlando", edad: 30 });
+pila.agregar({ nombre: "Luisa", edad: 22 });
+pila.agregar({ nombre: "Carlos", edad: 28 });
+
+pila.mostrar();
+pila.eliminar();
+pila.mostrar();
